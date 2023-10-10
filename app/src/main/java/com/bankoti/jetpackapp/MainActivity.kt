1fc992ce127123c1efc7a5d7b2b417570f7c0046
mikky <mikky.kumar30@gmail.com>
2023-10-10 11:21:17 +0530

    Commit 10/10/2023 workflow file with CICD

--- app/src/main/java/com/bankoti/jetpackapp/MainActivity.kt
+++ app/src/main/java/com/bankoti/jetpackapp/MainActivity.kt
@@ -5,8 +5,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 
 import com.bankoti.jetpackapp.apps.postOfficeApp
-
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
