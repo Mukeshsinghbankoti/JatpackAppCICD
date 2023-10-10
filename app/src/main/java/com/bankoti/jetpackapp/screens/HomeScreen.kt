c38caeca92c23152868e37d853107da659391f10
mikky <mikky.kumar30@gmail.com>
2023-10-10 10:55:22 +0530

    Commit 10/10/2023 workflow file with CICD

--- app/src/main/java/com/bankoti/jetpackapp/screens/HomeScreen.kt
+++ app/src/main/java/com/bankoti/jetpackapp/screens/HomeScreen.kt
@@ -1,23 +1,22 @@
 package com.bankoti.jetpackapp.screens
 
 import android.annotation.SuppressLint
-import android.content.Intent
+
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Call
-import androidx.compose.material.icons.filled.Clear
-import androidx.compose.material.icons.filled.Close
+
 import androidx.compose.material.icons.filled.ExitToApp
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.BottomAppBarDefaults
-import androidx.compose.material3.DrawerDefaults
+
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
@@ -42,11 +41,10 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
-import androidx.core.content.ContextCompat.startActivity
-import com.bankoti.jetpackapp.Components.MyAppBar
+
 import com.bankoti.jetpackapp.Components.NavigationDrawerHeader
 import com.bankoti.jetpackapp.data.NavigationItem
-import com.bankoti.jetpackapp.navigation.Screen
+
 import kotlinx.coroutines.launch
 
 
@@ -70,7 +68,6 @@ fun HomeScreen(){
         drawerContent = {
 
             ModalDrawerSheet {
-
                 NavigationDrawerHeader()
                 Spacer(Modifier.height(12.dp))
                 items.forEach { item ->
